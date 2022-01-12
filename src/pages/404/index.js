92ea9392150d277efbef97331399bc90dd24d5e2
chantelle mckenzie <mrspasceri@hotmail.com>
2022-01-12 15:25:31 -0800

    added 404 page, installed react-router-dom

---
+++ src/pages/404/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const index = () => {
+    return (
+        <div className='404'>
+            <div className='404-container'>
+                <Link to='/'>Back to Home Page</Link>
+            </div>
+            
+        </div>
+    );
+};
+
+export default index;
