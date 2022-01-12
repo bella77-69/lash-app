266e8ec0677b43485767a76418163ef23855c31f
chantelle mckenzie <mrspasceri@hotmail.com>
2022-01-12 14:41:19 -0800

    added Social Icons to Header.js, styled social.scss

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from '../../Assets/Logo/logo.png'
+import Social from "../Social/Social";
 import "./Header.scss";
 
 function Header() {
@@ -9,6 +10,7 @@ function Header() {
         <img className="header-img" src={logo} alt="lash-logo" className="header-logo"></img>
       </div>
       <div className="header-social">
+          <Social />
 
       </div>
     </section>
---
+++ src/components/Social/Social.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import './Social.scss';
+
+function Social() {
+  return (
+    <div className="social">
+      <p className="social-container">
+        <a href="https://www.facebook.com/" className="facebook social">
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+        <a href="http://www.instagram.com/" className="instagram social">
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
+        <a href="https://wwww.twitter.com" className="twitter social">
+          <FontAwesomeIcon icon={faTwitter} size="2x" />
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Social;
