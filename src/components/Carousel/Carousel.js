b07bd1f97a9d789bdad88b88b83fc6036821b217
chantelle mckenzie <mrspasceri@hotmail.com>
2022-01-12 21:47:32 -0800

    deleted css file

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default function CarouselComponent() {
-    return (
-        <div className="carousel-wrapper">
-            <Carousel>
-                <div>
-                    <img src="../img-01.jpg" />
-                </div>
-                <div>
-                    <img src="../img-02.jpg" />
-                </div>
-                <div>
-                    <img src="../img-03.jpg" />
-                </div>
-            </Carousel>
+  return (
+    <div className="carousel-wrapper">
+      <Carousel>
+        <div>
+          <img src="../img-01.jpg" />
         </div>
-    );
+        <div>
+          <img src="../img-02.jpg" />
+        </div>
+        <div>
+          <img src="../img-03.jpg" />
+        </div>
+      </Carousel>
+    </div>
+  );
 }
