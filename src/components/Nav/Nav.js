dad94d9e96394762678e0e411c531c1558b13f91
chantelle mckenzie <mrspasceri@hotmail.com>
2022-01-12 21:16:14 -0800

    formatted Nav, fixed up some styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,24 +1,35 @@
-import { slide as Menu } from 'react-burger-menu'
-import React from 'react';
-import './Nav.scss';
+import { slide as Menu } from "react-burger-menu";
+import React from "react";
+import "./Nav.scss";
 
 class Nav extends React.Component {
-    showSettings(event) {
-        event.preventDefault();
-    }
+  showSettings(event) {
+    event.preventDefault();
+  }
 
-    render() {
-        return (
-            <section className="menu">
-               <Menu>
-        <a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="/about">About</a>
-        <a id="contact" className="menu-item" href="/contact">Contact</a>
-        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-      </Menu>
+  render() {
+    return (
+      <section className="menu">
+        <Menu>
+          <a id="home" className="menu-item" href="/">
+            Home
+          </a>
+          <a id="about" className="menu-item" href="/about">
+            About
+          </a>
+          <a id="services" className="menu-item" href="/contact">
+            Services
+          </a>
+          <a id="contact" className="menu-item" href="/contact">
+            Contact
+          </a>
+          {/* <a onClick={this.showSettings} className="menu-item--small" href="">
+            Contact
+          </a> */}
+        </Menu>
       </section>
-        );
-    }
+    );
+  }
 }
 
 export default Nav;
