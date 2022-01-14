2a09697e0d331af4516628a4d16ef5e875c18796
chantelle mckenzie <mrspasceri@hotmail.com>
2022-01-13 21:57:50 -0800

    formatted Carousel.js

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,67 +1,73 @@
 import React from "react";
-import './Carousel.scss'
+import "./Carousel.scss";
 
 class Carousel extends React.Component {
-	constructor() {
-		super();
-		
-		this.state = {
-			currentIndex: 0,
-			isTransitioning: false,
-			goingLeft: false
-		};
-	}
-	
-	componentDidMount() {
-		window.addEventListener('keyup', this.onKeyUp);
-	}
-	
-	componentWillUnmount() {
-		window.removeEventListener('keyup', this.onKeyUp);
-	}
-	
-	onKeyUp = (e) => {
-		if (e.keyCode) {
-			if (e.keyCode === 39) {
-				this.showNextSet();
-			} else if (e.keyCode === 37) {
-				this.showPrevSet();
-			}
-		}
-	}
+  constructor() {
+    super();
 
-	render() {
+    this.state = {
+      currentIndex: 0,
+      isTransitioning: false,
+      goingLeft: false,
+    };
+  }
 
-		const { images } = this.props;
-		const { currentIndex, isTransitioning, goingLeft } = this.state;
-		
+  componentDidMount() {
+    window.addEventListener("keyup", this.onKeyUp);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keyup", this.onKeyUp);
+  }
+
+  onKeyUp = (e) => {
+    if (e.keyCode) {
+      if (e.keyCode === 39) {
+        this.showNextSet();
+      } else if (e.keyCode === 37) {
+        this.showPrevSet();
+      }
+    }
+  };
+
+  render() {
+    const { images } = this.props;
+    const { currentIndex, isTransitioning, goingLeft } = this.state;
+
     return (
-			<div className="carousel">
-				<div className="carousel-container">
-					{images.map((img, index) => {
-						let className = 'carousel-image'
-						if (index === currentIndex) className += '-active';
-						
-						return <img src={img} className={className} key={`img-${index}`} />;
-					})}
-				</div>
-				<div className="carousel-controls">
-					<button className="carousel-button" onClick={this.showPrevSet}>Prev<i className="fa fa-arrow-left"></i></button>
-					<button className="carousel-button" onClick={this.showNextSet}>Next<i className="fa fa-arrow-right"></i></button>
-				</div>
-			</div>
-		);
-	}
-	
-	showPrevSet = () => {
-		const currentIndex = (this.state.currentIndex - 1 + this.props.images.length) % this.props.images.length;
-		this.setState({ currentIndex });
-	}
-	
-	showNextSet = () => {
-		const currentIndex = (this.state.currentIndex + 1) % this.props.images.length;
-		this.setState({ currentIndex });
-	}
+      <div className="carousel">
+        <div className="carousel-container">
+          {images.map((img, index) => {
+            let className = "carousel-image";
+            if (index === currentIndex) className += "-active";
+
+            return <img src={img} className={className} key={`img-${index}`} />;
+          })}
+        </div>
+        <div className="carousel-controls">
+          <button className="carousel-button" onClick={this.showPrevSet}>
+            Prev<i className="fa fa-arrow-left"></i>
+          </button>
+          <button className="carousel-button" onClick={this.showNextSet}>
+            Next<i className="fa fa-arrow-right"></i>
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  showPrevSet = () => {
+    const currentIndex =
+      (this.state.currentIndex - 1 + this.props.images.length) %
+      this.props.images.length;
+    this.setState({ currentIndex });
+  };
+
+  showNextSet = () => {
+    const currentIndex =
+      (this.state.currentIndex + 1) % this.props.images.length;
+    this.setState({ currentIndex });
+  };
 }
 
 export default Carousel;
