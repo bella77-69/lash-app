a1293244a77ddecb9e1ede4c9aaa7416056e9047
chantelle mckenzie <mrspasceri@hotmail.com>
2022-01-23 19:32:41 -0800

    installed bcryptjs, cookie-parser, jsonwebtoken, and mongoose to server folder.  Connected MongoDB

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require("mongoose");
 
 app.use(cors());
 
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch((err) => console.log(err));
+
+  
 app.use('/login', (req, res) => {
     res.send({
       token: 'test123'
